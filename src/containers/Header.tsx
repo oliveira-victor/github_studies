ccdbac6d400fd23f878bb78208cc268aa6570f04
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-05-08 12:33:34 -0300

    adicionando header

---
+++ src/containers/Header.tsx
@@ -0,0 +1,13 @@
+import './header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <img src="https://static-00.iconduck.com/assets.00/github-inverted-icon-2048x1993-qoeck9vw.png" className="logo"
+                alt="Icone github" />
+            <h1>Estudos Github</h1>
+        </header>
+    )
+}
+
+export default Header
